090df07da89b034f0dfe69d0f29fad73b829aa3b
itc-n24002 <n24002@std.it-college.ac.jp>
2025-10-10 03:01:07 +0000

    1010kadai2

---
+++ m1_q4.py
@@ -0,0 +1,8 @@
+phrase = 'PythonProgramming'
+list_p = []
+for p in phrase:
+    if p not in list_p:
+        list_p.append(p)
+print(len(phrase) - len(list_p))
+for p in list_p:
+    print(p, end="")
